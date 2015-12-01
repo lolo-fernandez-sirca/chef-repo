24e6a7c928d77dd096613345da8134de6c690096
Lolo Fernandez <lolo.fernandez@sirca.org.au>
2015-12-01 14:25:57 +1100

    the book env

---
+++ environments/book.rb
@@ -0,0 +1,22 @@
+{
+  "name": "book",
+  "description": "",
+  "cookbook_versions": {
+    "ntp": "1.6.8"
+  },
+  "json_class": "Chef::Environment",
+  "chef_type": "environment",
+  "default_attributes": {
+
+  },
+  "override_attributes": {
+    "ntp": {
+      "servers": [
+        "0.europe.pool.ntp.org",
+        "1.europe.pool.ntp.org",
+        "2.europe.pool.ntp.org",
+        "3.europe.pool.ntp.org"
+      ]
+    }
+  }
+}
